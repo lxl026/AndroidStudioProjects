bc4aa247ca0c960f81b366e4bb4305de6f272e55
lxl026 <2470719811@qq.com>
2017-10-09 13:23:19 +0800

    first_commit

---
+++ Lab1/app/src/androidTest/java/company/leon/lab1/MainActivityTest.java
@@ -0,0 +1,16 @@
+package company.leon.lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Leon on 2017/9/19.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
